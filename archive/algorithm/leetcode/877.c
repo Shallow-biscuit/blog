eaa2207b4b5990393519eb570a1c0c79fb9866f1
Shallow_129 <35861615+Shallow-biscuit@users.noreply.github.com>
2025-04-28 16:16:52 +0800

    Delete archive directory

--- archive/algorithm/leetcode/877.c
+++
@@ -1,4 +0,0 @@
-bool stoneGame(int* piles, int pilesSize) {
-    return true;
-}
-//只要是我先，我就必胜，我拿最多的不就好了
--- archive/other/ssocket/readme.md
+++
@@ -1,9 +0,0 @@
-> 学习Linux c 挖坑
-
-## build
-
-```bash
-./autogen.sh && ./configure
-make
-```
-
